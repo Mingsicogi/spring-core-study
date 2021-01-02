5a0f7daa6df2fae8377f973b8fbcdfd902e1c292
Mingsicogi <codedoctor119@gmail.com>
2021-01-03 02:12:25 +0900

    Singleton vs Prototype Bean Scope

---
+++ src/mins/study/user/service/BeanScopeService.java
@@ -0,0 +1,19 @@
+package mins.study.user.service;
+
+public class BeanScopeService {
+
+    private String message;
+
+    public BeanScopeService(String message) {
+        this.message = message;
+        System.out.println("BeanScopeService Created. message : " + message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
