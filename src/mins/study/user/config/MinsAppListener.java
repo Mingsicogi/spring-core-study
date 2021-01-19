ccaf7033e0b90e57b3cce312794120eb6e393a37
Mingsicogi <codedoctor119@gmail.com>
2021-01-19 23:29:39 +0900

    Standard and Custom Events

---
+++ src/mins/study/user/config/MinsAppListener.java
@@ -0,0 +1,12 @@
+package mins.study.user.config;
+
+import mins.study.user.User;
+import org.springframework.context.ApplicationListener;
+
+public class MinsAppListener implements ApplicationListener<User> {
+
+    @Override
+    public void onApplicationEvent(User event) {
+
+    }
+}
